7057f96394538f4af8cfb1a25a377c383c1697e1
xdmingo <40335447+xdmingo@users.noreply.github.com>
2021-03-31 15:24:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## card-switcher
+* Changes card presets and collection effect when entering a dungeon
+### Instructions
+* Add defs to \TeraToolbox\data\definitions
+* Add opcodes to \TeraToolbox\data\opcodes
+* Set the IDs of your preset in `config.json`
+* Set the IDs of the base collection effect (defaultEffect = The one that is selected all the time, secondaryEffect = This one will be selected if you enter a dungeon with a boss of no type)
+
+Supported dungeons:
+* CKG
+* KG
+* CATN
+* CATH
+* FA
+* CSNM
+* CSHM
+* Bahaar
+* DA
+* AQ
+* GV and RMN (basic preset and effect only)
+
+Opcodes for EU
+
+DM on discord if help needed: Mingo The Slump God#0001
+
+I succ at coding, yes
---
+++ index.js
@@ -0,0 +1,66 @@
+const config = require('./config.json');
+let zoneId;
+
+module.exports = function CardPresets(mod) {
+
+    mod.hook('S_LOAD_TOPO', 3, (event) => {
+        zoneId = event.zone
+
+        if(zoneId == 3206 || zoneId == 3106 || zoneId == 3103) //ancestor
+        {
+            setPresetAndEffects(33, config.ancestorPreset -1)
+        }
+        else if(zoneId == 3104 || zoneId == 3204 || zoneId == 3102) //azart
+        {
+            setPresetAndEffects(28, config.azartPreset -1)
+        }
+        else if(zoneId == 9970) //demon
+        {
+            setPresetAndEffects(32, config.demonPreset -1)
+        }
+        else if(zoneId == 3026 || zoneId == 3126 || zoneId == 3023) //argon
+        {
+            setPresetAndEffects(24, config.argonPreset -1)
+        }
+        else if(zoneId == 9044) //god
+        {
+            setPresetAndEffects(26, config.godPreset -1)
+        }
+        else if(zoneId == 'placeholder') //mag creature (none yet)
+        {
+            setPresetAndEffects(29, config.magicalPreset -1)
+        }
+        else 
+        {
+            setPresetAndEffects(config.secondaryEffect, config.basicPreset -1)
+        }
+
+    });
+
+  function setPresetAndEffects(colId, presetId)
+  {
+
+    clearEffects();
+    mod.toServer('C_ACTIVATE_CARD_COMBINE_LIST', 1, {
+        id: colId
+    })
+    mod.toServer('C_CHANGE_CARD_PRESET', 1, {
+        preset: presetId
+    })
+  }
+
+  function clearEffects()
+  {
+     for (let i = 0; i < 34; i++) {
+         
+        mod.toServer('C_DEACTIVATE_CARD_COMBINE_LIST', 1, {
+            id: i
+        })
+         
+     }
+     mod.toServer('C_ACTIVATE_CARD_COMBINE_LIST', 1, {
+        id: config.defaultEffect
+    })
+  }
+
+}
